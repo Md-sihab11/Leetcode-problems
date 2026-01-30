ba0c592aac77cbd05f7688304969892f143caf15
MD Shakib <mdsihabsharar22@gmail.com>
2026-01-30 23:13:05 +0600

    New Solved

--- Leetcode/Solution1.java
+++ Leetcode/Solution1.java
@@ -1,23 +1,20 @@
 package Leetcode_Solved_problems.Leetcode;
-import java.util.*;
 
+import java.util.*;
 
 //56. Merged Intervals
 public class Solution1 {
 
     public static int[][] merge(int[][] intervals) {
 
-        // Step 1: sort by start time
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> merged = new ArrayList<>();
-
         int[] prev = intervals[0];
 
         for (int i = 1; i < intervals.length; i++) {
             int[] curr = intervals[i];
 
-            // overlap check
             if (curr[0] <= prev[1]) {
                 prev[1] = Math.max(prev[1], curr[1]);
             } else {
@@ -25,27 +22,24 @@ public class Solution1 {
                 prev = curr;
             }
         }
-
         merged.add(prev);
-
         return merged.toArray(new int[merged.size()][]);
     }
 
-    // MAIN method (IntelliJ run korar jonno)
     public static void main(String[] args) {
 
         int[][] intervals = {
-                {1, 3},
-                {2, 6},
-                {8, 10},
-                {15, 18}
+            {1, 3},
+            {2, 6},
+            {8, 10},
+            {15, 18}
         };
 
         int[][] result = merge(intervals);
-
         System.out.println("Merged Intervals:");
         for (int[] arr : result) {
             System.out.println(Arrays.toString(arr));
         }
+
     }
 }

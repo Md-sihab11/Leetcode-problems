df222a7bb9a2470fe7fa1a97f1339dac44234754
MD Shakib <mdsihabsharar22@gmail.com>
2026-02-04 17:22:48 +0600

    Coforces

---
+++ SimpleCondition.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//4/6/26
+int main()
+{
+    int var = 80;
+
+    //condition ? value_if_true : value_if_false
+
+    cout<< (var%2==0 && var>0 ? "YES":"NO");
+    return 0;
+}
